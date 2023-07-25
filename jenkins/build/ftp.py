30a89007799941e5001437ce7231d64464ce3782
suraphop <mic@admin.com>
2023-07-25 09:25:30 +0700

    first commit

---
+++ jenkins/build/ftp.py
@@ -0,0 +1,44 @@
+import ftplib
+from datetime import datetime, timedelta
+
+
+def ftp_upload(project_path,file):
+    try:
+        hostname = '192.168.100.11'
+        user = 'test'
+        pwd = '1234'
+
+        now = datetime.now()
+        date_file_name = f'{str(now.date())}_{str(now.time()).split(".")[0].replace(":","_")}'
+        year,month,date = mfg_date()
+
+
+        upload_path = f"/{project_path}/{year}/{month}/{date}/TYLALOND_{date_file_name}.csv"
+
+        session = ftplib.FTP(hostname,user,pwd)
+
+        session.mkd(f'{project_path}/{year}')
+        session.mkd(f'{project_path}/{year}/{month}')
+        session.mkd(f'{project_path}/{year}/{month}/{date}')
+
+        session.storbinary(f'STOR {upload_path}', file) 
+
+        file.close()                                
+        session.quit()
+        return {"result":"ok",'code':'-'}
+    except Exception as e: 
+        return {"result":"error",'code':e}
+
+def mfg_date():
+    mfg_date = datetime.now() - timedelta(hours=7)
+    year = mfg_date.year
+    month = mfg_date.month
+    date = mfg_date.day
+
+    if len(str(year)) == 1:
+        year = '0'+str(year)
+    if len(str(month)) == 1:
+        month = '0'+str(month)
+    if len(str(date)) == 1:
+        date = '0'+str(date)
+    return year,month,date
